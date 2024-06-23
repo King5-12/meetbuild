d199227d75362da92b051970400e6aebc69fdd2f
kim <jinweizhong@dragonest.com>
2024-06-24 02:38:01 +0800

    fix: 增加日志

--- src/buildBack.js
+++ src/buildBack.js
@@ -1,61 +1,47 @@
 const execa = require('execa');
-const { BACK_PATH } = require("./constant")
+const { BACK_PATH } = require('./constant');
 const ora = require('ora');
 const stringRandom = require('string-random');
 
-dockerName = ""
+dockerName = '';
 const spinner = ora();
 
 const refreshDockerName = () => {
-    dockerName = stringRandom()
-}
+  dockerName = stringRandom();
+};
 
 const buildBack = async () => {
-    spinner.start(`更新最新代码`);
-    await execa('git', [
-        'pull',
-    ], {
-        cwd: BACK_PATH,
-    });
-    spinner.succeed();
+  spinner.start(`更新 buildBack 最新代码`);
+  await execa('git', ['pull'], {
+    cwd: BACK_PATH,
+  });
+  spinner.succeed();
 
-    if (dockerName) {
-        spinner.start(`开始结束上一个镜像`);
-        await execa('docker', [
-            'stop',
-            dockerName
-        ], {
-            cwd: BACK_PATH,
-        });
-        refreshDockerName()
-        spinner.succeed();
-    } else {
-        refreshDockerName()
+  if (dockerName) {
+    spinner.start(`开始结束上一个镜像 buildBack`);
+    await execa('docker', ['stop', dockerName], {
+      cwd: BACK_PATH,
+    });
+    refreshDockerName();
+    spinner.succeed();
+  } else {
+    refreshDockerName();
+  }
+
+  spinner.start(`开始构建镜像 buildBack`);
+  await execa('docker', ['build', '-t', 'meetback', '.'], {
+    cwd: BACK_PATH,
+  });
+  spinner.succeed();
+  spinner.start(`开始发布镜像 buildBack`);
+  await execa(
+    'docker',
+    ['run', '-p', '8080:8080', '--name', dockerName, '-itd', 'meetback'],
+    {
+      cwd: BACK_PATH,
     }
-
-    spinner.start(`开始构建镜像`);
-    await execa('docker', [
-        'build',
-        '-t',
-        'meetback',
-        '.',
-    ], {
-        cwd: BACK_PATH,
-    });
-    spinner.succeed();
-    spinner.start(`开始发布镜像`);
-    await execa('docker', [
-        'run',
-        '-p',
-        '8080:8080',
-        '--name',
-        dockerName,
-        '-itd',
-        'meetback',
-    ], {
-        cwd: BACK_PATH,
-    });
-    spinner.succeed();
-}
+  );
+  spinner.succeed();
+};
 
 module.exports = buildBack;
--- src/buildNextEditServer.js
+++ src/buildNextEditServer.js
@@ -11,14 +11,14 @@ const refreshDockerName = () => {
 };
 
 const buildNextEditServer = async () => {
-  spinner.start(`更新最新代码`);
+  spinner.start(`更新最新代码 buildNextEditServer`);
   await execa('git', ['pull'], {
     cwd: NEXT_EDIT_PATH,
   });
   spinner.succeed();
 
   if (dockerName) {
-    spinner.start(`开始结束上一个镜像`);
+    spinner.start(`开始结束上一个镜像 buildNextEditServer`);
     await execa('docker', ['stop', dockerName], {
       cwd: NEXT_EDIT_PATH,
     });
@@ -28,12 +28,12 @@ const buildNextEditServer = async () => {
     refreshDockerName();
   }
 
-  spinner.start(`开始构建镜像`);
+  spinner.start(`开始构建镜像 buildNextEditServer`);
   await execa('docker', ['build', '-t', 'nextEditServer', '.'], {
     cwd: NEXT_EDIT_PATH,
   });
   spinner.succeed();
-  spinner.start(`开始发布镜像`);
+  spinner.start(`开始发布镜像 buildNextEditServer`);
   await execa(
     'docker',
     ['run', '-p', '3001:3001', '--name', dockerName, '-itd', 'nextEditServer'],
